33ea7f886569b43f7235633e0ef10e0689a7f66b
Hendrik Sollich <hendrik@hoodie.de>
2020-01-16 12:44:55 +0100

    fixes and cleanups

---
+++ benches/lib.rs
@@ -0,0 +1,33 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+use test::black_box;
+
+extern crate zummi;
+use zummi::{zummi, zummi_broke, zummi_naive};
+
+#[bench]
+fn zummi_bench(b: &mut Bencher) {
+    b.iter(|| {
+        let mix = zummi("stinkender fisch");
+        black_box(mix);
+    });
+}
+
+#[bench]
+fn zummi_broke_bench(b: &mut Bencher) {
+    b.iter(|| {
+        let mix = zummi_broke("stinkender fisch");
+        black_box(mix);
+    });
+}
+
+#[bench]
+fn zummi_naive_bench(b: &mut Bencher) {
+    b.iter(|| {
+        let mix = zummi_naive("stinkender fisch");
+        black_box(mix);
+    });
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-#![cfg_attr(feature = "test", feature(test))]
-
-#[cfg(feature = "nightly")]
-#[cfg(test)]
-extern crate test;
-
 const VOWELS: &str = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
 const CONSONANTS: &str = "aeiouyAEIOUY";
 
 fn stem(word: &str) -> (String, String) {
-    let start = word.chars().take_while(|c| VOWELS.contains(*c)).collect::<String>();
+    let start = word
+        .chars()
+        .take_while(|c| VOWELS.contains(*c))
+        .collect::<String>();
     let end = word.chars().skip(start.len()).collect::<String>();
     (start, end)
 }
@@ -42,6 +39,29 @@ pub fn zummi_naive(phrase: &str) -> Option<String> {
     }
 }
 
+/// Broken version where I accidentally cause an unnecessary allocation.
+///
+/// # Example
+///
+/// ```
+/// extern crate zummi;
+/// assert_eq!(zummi::zummi_broke("hello world"), Some(String::from("wello horld")));
+/// ```
+///
+pub fn zummi_broke(phrase: &str) -> Option<String> {
+    let mut words = phrase.split_whitespace();
+    let (first, second) = (words.next(), words.next());
+    if let (Some(first), Some(second)) = (first, second) {
+        let (f, irst) = stem2(first);
+        let (s, econd) = stem2(second);
+        let capacity = f.len() + irst.len() + s.len() + econd.len();
+        let spoonerism = String::with_capacity(capacity) + s + irst + " " + f + econd;
+        Some(spoonerism)
+    } else {
+        None
+    }
+}
+
 /// Splits a phase of two words and shuffles their beginnings.
 ///
 /// # Example
@@ -57,35 +77,10 @@ pub fn zummi(phrase: &str) -> Option<String> {
     if let (Some(first), Some(second)) = (first, second) {
         let (f, irst) = stem2(first);
         let (s, econd) = stem2(second);
-        let capacity = f.len() + irst.len() + s.len() + econd.len();
+        let capacity = f.len() + irst.len() + s.len() + econd.len() + " ".len();
         let spoonerism = String::with_capacity(capacity) + s + irst + " " + f + econd;
         Some(spoonerism)
     } else {
         None
     }
-}
-
-#[cfg(test)]
-#[cfg(feature = "nightly")]
-mod tests {
-    use super::*;
-    use test::black_box;
-    use test::Bencher;
-
-    #[bench]
-    fn zummi_bench(b: &mut Bencher) {
-        b.iter(|| {
-            let mix = zummi("stinkender fisch");
-            black_box(mix);
-        });
-    }
-
-    #[bench]
-    fn zummi_naiv_bench(b: &mut Bencher) {
-        b.iter(|| {
-            let mix = zummi_naiv("stinkender fisch");
-            black_box(mix);
-        });
-    }
-
 }
